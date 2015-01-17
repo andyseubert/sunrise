20b27ee7670c68a73dbeaae810d9d207002e0d1e
andyseubert <andys@marginalventures.com>
2015-01-17 14:56:32 -0800

    making stuff

--- get_sunrise.sh
+++ get_sunrise.sh
@@ -2,4 +2,4 @@
 
 wget -O sr.html "http://aa.usno.navy.mil/rstt/onedaytable?form=1&ID=AA&year=2015&month=1&day=10&state=OR&place=portland"
 
-grep Sunrise sr.html | cut -d">" -f5 | cut -d"<" -f1 > sunrisetime
+grep Sunrise sr.html | cut -d">" -f5 | cut -d"<" -f1 | cut -d " " -f 1 > sunrisetime
