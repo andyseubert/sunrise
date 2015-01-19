e8eea8f2f870c39996346802aae88bc8be7ef3e1
andyseubert <andys@marginalventures.com>
2015-01-18 21:46:07 -0800

    getting it going

--- get_sunrise.sh
+++ get_sunrise.sh
@@ -1,13 +1,33 @@
 #! /bin/bash
 
-wget -O sr.html "http://aa.usno.navy.mil/rstt/onedaytable?form=1&ID=AA&year=2015&month=1&day=10&state=OR&place=portland"
+
+DOM=$(date +"%-d")
+MON=$(date +"%-m")
+YEAR=$(date +"%Y")
+
+wget -O sr.html "http://aa.usno.navy.mil/rstt/onedaytable?form=1&ID=AA&year=$YEAR&month=$MON&day=$DOM&state=OR&place=portland"
+cp sr.html /var/www/
 
 SUNUP=$(grep Sunrise sr.html | cut -d">" -f5 | cut -d"<" -f1 | cut -d " " -f 1) # > sunrisetime
-M=$SUNUP | cut -d ":" -f 1
-M=$M - 1
-H=$SUNUP \ cut -d ":" -f 2
-DOM=date +"%d"
-MON=date +"%m"
-
+M=$(echo $SUNUP | cut -d ":" -f 1)
+let "M=$M-1"
+H=$(echo $SUNUP | cut -d ":" -f 2)
 # m h  dom mon dow   command
-CRONLINE=$M $H $DOM $MON * /usr/local/sunrise/snapshot.sh
+CRONLINE="$M $H $DOM $MON * /usr/local/sunrise/snapshot.sh"
+
+# copy the original crontab file to tmp
+
+cp /usr/local/sunrise/rawcron.file /tmp/cron
+
+# put my daily cron line into the temp file
+echo "$CRONLINE" >> /tmp/cron 
+
+# make crontab use the temp file for crontab
+crontab -u root /tmp/cron
+
+MOONIMG=$(grep image sr.html |cut -d "<" -f 3 | cut -d ">" -f 1 )
+IFS=$'\n' read -d '' -r -a LINK <<< "$MOONIMG"
+HREF=$(echo $MOONIMG | cut -d "\"" -f 2)
+MOONPHASE="<a href="$HREF"<"$LINK" /></a>"
+
+echo "$HREF" | mail -s "moon phase" andys\@florapdx.com
--- snapshot.sh
+++ snapshot.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-# check if it's sunrise now
-# if not exit
+# this will get run from crontab each day 
+
 LIGHTTPD="/var/www/"
 # make a folder with today's date in the lighttpd root dir
 DATE=$(date +"%Y-%m-%d_%H%M")
@@ -15,3 +15,11 @@ mkdir $DEST
 
 raspistill -tl 60000 -t 7200000 -o $DEST/sunrise_%04d.jpg
 
+# use ffmpeg to make a video
+# store it in the root of the lighttpd directory
+ffmpeg -r 12 -i $DEST/sunrise_%04d.jpg -sameq -s hd720 -vcodec libx264 -crf 25 $LIGHTTPD$DATE.MP4
+
+## make an html page to contain the videos
+IP=$(/sbin/ifconfig | grep "inet addr" | grep -v 127.0.0.1|cut -d ":" -f 2 | cut -d " " -f 1)
+BODY="http://$IP/$DATE"
+echo "$BODY" | mail -s "new sunrise ready" andys\@florapdx.com
