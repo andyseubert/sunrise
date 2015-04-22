77fe586c8ab47f49f9eccb96479859e3c12e04f4
andyseubert <andys@marginalventures.com>
2015-04-22 10:21:51 -0700

    broke individual file preservation fixed

--- snapshot.sh
+++ snapshot.sh
@@ -18,7 +18,7 @@ raspistill -tl 60000 -t 7200000 -o $DEST/sunrise_%04d.jpg
 # use ffmpeg to make a video
 # store it in the root of the lighttpd directory
 #ffmpeg -i $DEST/sunrise_%04d.jpg -r 10 -vcodec libx264 -crf 25 $LIGHTTPD$DATE.MP4
-ffmpeg -r 6 -f image2 -i $DEST/sunrise_%04d.jpg -vcodec mjpeg -qscale 1 -pix_fmt yuv420p $LIGHTTPD$DATE.avi
+ffmpeg -r 6 -f image2 -i $DESTDIR/sunrise_%04d.jpg -vcodec mjpeg -qscale 1 -pix_fmt yuv420p $LIGHTTPD$DATE.avi
 
 ## make an html page to contain the videos
 IP=$(/sbin/ifconfig | grep "inet addr" | grep -v 127.0.0.1|cut -d ":" -f 2 | cut -d " " -f 1)
@@ -33,6 +33,6 @@ echo "<a href=$DESTDIR><br>$DESTDIR</a><BR>" >> $LIGHTTPD/index.html
 scp -i /home/andys/.ssh/tunnel-id $LIGHTTPD$DATE.avi florapor@floraportland.com:$DST
 scp -i /home/andys/.ssh/tunnel-id $LIGHTTPD/index.html florapor@floraportland.com:$DST
 ## scp the directory
-scp -ri /home/andys/.ssh/tunnel-id $DEST florapor@floraportland.com:$DST
+scp -ri /home/andys/.ssh/tunnel-id $DESTDIR florapor@floraportland.com:$DST
 echo "http://mythingonthe.net/$DATE.avi" | mail -s "new sunrise video link ready" andys\@florapdx.com
 
