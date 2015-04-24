9bc9b539b34ced8fa1c1b3fb80ab0809414cfe5b
andyseubert <andys@marginalventures.com>
2015-04-24 09:24:02 -0700

    enloggening the video

--- snapshot.sh
+++ snapshot.sh
@@ -7,18 +7,19 @@ LIGHTTPD="/var/www/"
 DATE=$(date +"%Y-%m-%d_%H%M")
 
 # set that as destination for snapshots
-DESTDIR=$(date +"%Y-%m-%d")
-mkdir $LIGHTTPD$DESTDIR
+DEST=$(date +"%Y-%m-%d")
+mkdir $LIGHTTPD$DEST
+DESTDIR=$LIGHTTPD$DEST
 
 # take snapshots once per minute for one hour
 # name them with the time taken
 
-raspistill -tl 60000 -t 7200000 -o $DEST/sunrise_%04d.jpg
+raspistill -tl 60000 -t 7200000 -o $DESTDIR/sunrise_%04d.jpg
 
 # use ffmpeg to make a video
 # store it in the root of the lighttpd directory
 #ffmpeg -i $DEST/sunrise_%04d.jpg -r 10 -vcodec libx264 -crf 25 $LIGHTTPD$DATE.MP4
-ffmpeg -r 6 -f image2 -i $DESTDIR/sunrise_%04d.jpg -vcodec mjpeg -qscale 1 -pix_fmt yuv420p $LIGHTTPD$DATE.avi
+ffmpeg -r 3 -f image2 -i $DESTDIR/sunrise_%04d.jpg -vcodec mjpeg -qscale 1 -pix_fmt yuv420p $LIGHTTPD$DATE.avi
 
 ## make an html page to contain the videos
 IP=$(/sbin/ifconfig | grep "inet addr" | grep -v 127.0.0.1|cut -d ":" -f 2 | cut -d " " -f 1)
