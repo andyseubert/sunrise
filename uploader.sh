2c7f1625fd3ff2630b5c6d7094882879bea03cd3
Andrew Seubert <andys@marginalventures.com>
2015-10-27 15:28:38 -0700

    Update uploader.sh
    Added checking if video was pulled down. If not it emails an alert

--- uploader.sh
+++ uploader.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
+#/root/sendsms.py "starting uploader"
+#echo "starting upload" | mail -s "starting upload" andys@florapdx.com
 
-# cd to temp dir
+
 mkdir /tmp/sunrises
 cd /tmp/sunrises
 
 # get today's avi from the server
-wget -nH -rA "$(date +"%Y-%m-%d")*.avi" http://yourthingonthe.net
-
-#get all avis from the server
-#wget -nH -rA "*.avi" http://yourthingonthe.net
-
-# rename it or them (the sunrise capture adds hour and minute to the filename)
+wget -nH -rA "$(date +"%Y-%m-%d")*.avi" http://mythingonthe.net
+if [ -e $(date +"%Y-%m-%d")*.avi ]
+ then echo "exists"
+# rename them
 for X in $(ls); do
 FILE=$(echo $X|cut -d "_" -f 1)
 echo "$FILE.avi"
@@ -20,11 +20,16 @@ done
 ## upload to youtube
 for X in $(ls); do
 TEXT=$(date -d $(echo $X|cut -d "." -f 1) +"%B %d %Y")
-youtube-upload -m you@you.com -p ********** -t "sunrise $TEXT" -c "Entertainment" $X
+/usr/local/bin/youtube-upload -m andys@florapdx.com -p $PASS -t "Portland Oregon sunrise $TEXT" -c "Entertainment" -d "https://github.com/andyseubert/sunrise" $X
+
+
+else
+ echo "boo hoo"
+ echo "failed to pull down video for today $(date +"%Y-%m-%d") "| mail -s "failed upload" andys@florapdx.com
+fi
+
+cd /root
+
 sleep 90
 rm -rf /tmp/sunrises
 done
-
-
-
-
