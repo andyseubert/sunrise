c49d7b873156ca5445f74cf5b4e17e04e6f6c06f
Andrew Seubert <andys@marginalventures.com>
2015-11-02 13:03:27 -0800

    had the filename withoug the extension

--- uploader.sh
+++ uploader.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#! /bin/bash 
 #/root/sendsms.py "starting uploader"
-#echo "starting upload" | mail -s "starting upload" andys@florapdx.com
-
+echo "starting upload" | mail -s "starting upload" andys@florapdx.com
+PASS="ewzlavfuncneghwv"
 
 mkdir /tmp/sunrises
 cd /tmp/sunrises
@@ -9,21 +9,18 @@ cd /tmp/sunrises
 # get today's avi from the server
 wget -nH -rA "$(date +"%Y-%m-%d")*.avi" http://mythingonthe.net
 if [ -e $(date +"%Y-%m-%d")*.avi ]
- then echo "exists"
-# rename them
-for X in $(ls); do
-FILE=$(echo $X|cut -d "_" -f 1)
-echo "$FILE.avi"
-mv $X $FILE.avi
-done
-
+then
+ echo "exists"
+ # rename them
+ for X in $(ls); do
+  FILE=$(echo $X|cut -d "_" -f 1)
+  echo "$FILE.avi"
+  mv $X $FILE.avi
+ done
 ## upload to youtube
-for X in $(ls); do
-TEXT=$(date -d $(echo $X|cut -d "." -f 1) +"%B %d %Y")
-/usr/local/bin/youtube-upload -m andys@florapdx.com -p $PASS -t "Portland Oregon sunrise $TEXT" -c "Entertainment" -d "https://github.com/andyseubert/sunrise" $X
-
-
-else
+ TEXT=$(date -d $(echo $FILE|cut -d "." -f 1) +"%B %d %Y")
+ /usr/local/bin/youtube-upload -m andys@florapdx.com -p $PASS -t "Portland Oregon sunrise $TEXT" -c "Entertainment" -d "https://github.com/andyseubert/sunrise" $FILE.avi
+else 
  echo "boo hoo"
  echo "failed to pull down video for today $(date +"%Y-%m-%d") "| mail -s "failed upload" andys@florapdx.com
 fi
@@ -32,4 +29,5 @@ cd /root
 
 sleep 90
 rm -rf /tmp/sunrises
-done
+
+
